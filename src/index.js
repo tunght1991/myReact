ac435e8280aba8be96487fa42ea280ed3873dee7
tunght1991 <tungutc91@gmail.com>
2017-03-24 10:51:08 +0700

    add all file todo app list

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TodoApp from './containers/TodoApp';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import rootReducer from './reducers/rootReducers';
 import './index.css';
 
+const initialState = {}
+
+const store = createStore(rootReducer, initialState);
+
+const appRoot = (
+  <Provider store={store}>
+    <div>
+      <TodoApp />
+    </div>
+  </Provider>
+)
 ReactDOM.render(
-  <App />,
+  appRoot,
   document.getElementById('root')
 );
